3fcf2ae508e9105bb2629eee78d7fb0542dc4d39
LongVN99 <LongVn1999vn@yahoo.com>
2021-04-30 15:10:01 +0000

    Added Subscribe

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+from .models import SubscribeUser
+
+class SubscribeUserSignUpForm(forms:ModelForm):
+    class Meta:
+        model = SubscribeUser
+        fields = ['email']
+
+        def standard_email(self):
+            email = self.standard_email.get('email')
+
+            return email 
