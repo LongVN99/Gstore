241177535b2b8e37578b125ad0fa33ac85d16fe2
LongVN99 <LongVn1999vn@yahoo.com>
2021-04-30 15:21:15 +0000

    Added subscribe views

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from .models import SubscribeUser
+from .forms import SubscribeUserSignUpForm
 # Create your views here.
+
+def subscibe_signup(request):
+    form = SubscribeUserSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        isinstance = form.save(commit=False)
+        if SubscribeUser.objects.filter(email=isinstance.email).exists():
+            print("Email is already exist!")
+        else: 
+            isinstance.save()
+
+    context = {
+        'form': form,
+    }
+    template = 'subscribe/subscribe.html'
+
+    return render(request, template, context)
+
